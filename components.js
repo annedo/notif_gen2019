c27edc4509de28cf0442e9d98f22283da848260e
Anne Do <anne.do.designs@gmail.com>
2020-02-11 14:56:18 -0500

    Getting better at Vue

--- components.js
+++ components.js
@@ -1,95 +1,47 @@
-Vue.component('viet-notification', {
-    props: ['formValues'],
-    computed: {
-        templateContent: function() {
-            console.log(this.formValues);
-            if (this.formValues.notifType === 'documents') {
-                return 'documents'
-            }
-            else if (this.formValues.notifType === 'payment') {
-                if (!this.formValues.paid && !this.formValues.validPayment) {
-                    return 'unpaid_invalid';
-                }
-                else if (!this.formValues.paid && this.formValues.validPayment) {
-                    return 'unpaid_valid';
-                }
-                else if (this.formValues.paid && this.formValues.autopay) {
-                    return 'paid_autopay';
-                }
-                else if (this.formValues.paid && !this.formValues.autopay) {
-                    return 'paid_manual'
-                }
-            }
-        },
-        clientGender: function() {
-            return this.$root.clientGender;
-        }
-    },
-    /*<p v-if="templateContent === 'documents'">
-        Chào {{clientGender}} {{clientName}}, Obamacare của {{clientGender}} cần bổ sung thêm giấy tờ. Làm ơn nhắn tin hình {{reqsStrViet}}. Nếu không gửi trước khì {{dueDate}}, giá bảo hiểm của {{clientGender}} sẽ tăng lên. Liên lạc Thuy Bell 727-280-4563. Cám ơn!
-    </p>*/
-    template: `
-    <p v-else-if="templateContent === 'unpaid_invalid'">
-        Chào {{clientGender}} {{clientName}}, {{company}} chưa nhận được tiền {{clientGender}}, và Thuy Bell không xài được thẻ nhà băng {{clientGender}} (nếu {{clientGender}} đã cho). Nếu không trả trước khì ngày hạn chót, bảo hiểm của {{clientGender}} sẽ bị hủy bỏ.
-
-    <br/><br/>Mỗi tháng trả: \${{premium}}. 
-    <br/>{{capitalize(clientGender)}} nợ: 2 tháng, là \${{total}}. 
-    <br/>Ngày hạn chót: {{dueDate}}
-
-    <br/><br/>Nếu {{clientGender}} có câu hỏi hay cần giúp, gọi/nhắn tin Thuy Bell 727-280-4563. Cám ơn!
-    </p>
-
-    <p v-else-if="templateContent === 'unpaid_valid'">
-        Chào {{clientGender}} {{clientName}}, {{company}} chưa nhận được tiền {{clientGender}}. Nếu {{clientGender}} không trả trước khì ngày hạn chót, bảo hiểm của {{clientGender}} sẽ bị hủy bỏ.
-
-    <br/><br/>Mỗi tháng trả: \${{premium}}. 
-    <br/>{{capitalize(clientGender)}} nợ: 2 tháng, là \${{total}}. 
-    <br/>Ngày hạn chót: {{dueDate}}
-
-    <br/><br/>**{{capitalize(clientGender)}} có muốn Thuy Bell trả bằng thẻ nhà băng {{clientGender}} đã cho, trả lời YES.
-
-    <br/><br/>Nếu có câu hỏi, gọi/nhắn tin Thuy Bell 727-280-4563. Cám ơn!
-    </p>
-
-    <p v-else-if="templateContent === 'paid_autopay'">
-        Chào {{clientGender}} {{clientName}}, Thuy Bell đã trả tiền còn nợ (\${{total}}) cho {{company}} bằng thẻ nhà băng {{clientGender}} đã cho. {{capitalize(clientGender)}} có Autopay, là tự động rút \${{premium}} chên thẻ nhà băng {{clientGender}} mỗi tháng ngày 16, bắt đầu Feb 16. Nếu cần thay đổi gì hay bỏ hủy Autopay, thông báo Thuy liền cho tránh phí tiềm năng và mất bảo hiểm. Nếu {{clientGender}} có câu hỏi gọi/tin nhắn Thuy Bell 727-280-4563. Cám ơn!
-    </p>
-
-    <p v-else-if="templateContent === 'paid_manual'">
-        Chào {{clientGender}} {{clientName}}, Thuy Bell đã trả tiền còn nợ (\${{total}}) cho {{company}} bằng thẻ nhà băng {{clientGender}} đã cho. Mỗi tháng {{clientGender}} sẽ nhận thư cho \${{premium}}. {{capitalize(clientGender)}} có trách nhiệm tự trả. Nếu {{clientGender}} muốn Autopay (tự động trả) hay có câu hỏi, gọi/tin nhắn Thuy Bell 727-280-4563. Cám ơn!
-    </p>
-    `
-});
-
 var app = new Vue({
     el: '#app',
     data: {
-        clientName: 'NAME',
-        notifType: 'payment',
-        clientGender: 'chị',
-        company: 'Oscar Insurance',
-        premium: '5',
-        total: '10',
+        notifCategory: 'documents',
         paid: false,
         autopay: false,
         validPayment: false,
+        clientName: '',
+        clientGender: 'chị',
+        company: 'Ambetter',
+        premium: '5',
+        total: '',
+        dueDate: new Date(new Date().getFullYear(), new Date().getMonth()+1, 0).toISOString().substring(0,10),
+        familyMembers: [{ 
+            name: '',
+            req_income: false,
+            req_ssn: false,
+            req_citizenship: false,
+            req_greencard: false,
+            req_letter: false
+        }]
     },
     computed: {
-        dueDate: function() {
-            var today = new Date();
-            var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
-            return lastDayOfMonth.toISOString().substring(0,10);
+        displayDueDate: function() {
+            var displayDate = new Date(this.dueDate).toLocaleDateString('en-us', {timeZone: 'UTC'});
+
+            return displayDate;
         },
-        formValues: function() {
-            return {
-                notifType: this.notifType,
-                paid: this.paid,
-                validPayment: this.validPayment,
-                autopay: this.autopay
+        displayPremium: function() {
+            return this.normalizeDollars(this.premium);
+        },
+        displayTotal: function() {
+            if (this.total == '') {
+                return (this.displayPremium*2).toFixed(2);
             }
-        }
+            else {
+                return this.normalizeDollars(this.total);
+            }
+        },
     },
     methods: {
+        normalizeDollars: function(str) {
+            return parseFloat(str.replace(/\$/, "")).toFixed(2);
+        },
         capitalize: function(str) {
             return str[0].toUpperCase() + str.slice(1);
         },
@@ -103,6 +55,9 @@ var app = new Vue({
             tempInput.select();
             document.execCommand("copy");
             document.body.removeChild(tempInput);
+        },
+        getMemberId: function(index) {
+            return `member${index}`;
         }
     }
 })
@@ -112,28 +67,28 @@ var app = new Vue({
         index: Number
     },
     template: 
-        `<fieldset id="member{{index}}" class="memberInfo">
-            <legend>Missing Member {{index}}:</legend>
+        `<fieldset id="member{{notifValues.index}}" class="memberInfo">
+            <legend>Missing Member {{notifValues.index}}:</legend>
             <div>
-                <label for="member{{index}}_name">Name:</label>
-                <input type="text" name="member{{index}}_name" id="member{{index}}_name">
+                <label for="member{{notifValues.index}}_name">Name:</label>
+                <input type="text" name="member{{notifValues.index}}_name" id="member{{notifValues.index}}_name">
             </div>
             <fieldset>
                 <legend>Documents:</legend>
                 <div>
-                    <input name="member{{index}}_docs" type="checkbox" id="member{{index}}_income" value="income"><label for="member{{index}}_income">Income</label>
+                    <input name="member{{notifValues.index}}_docs" type="checkbox" id="member{{notifValues.index}}_income" value="income"><label for="member{{notifValues.index}}_income">Income</label>
                 </div>
                 <div>
-                    <input name="member{{index}}_docs" type="checkbox" id="member{{index}}_ssn" value="SSN"><label for="member{{index}}_ssn">SSN</label>
+                    <input name="member{{notifValues.index}}_docs" type="checkbox" id="member{{notifValues.index}}_ssn" value="SSN"><label for="member{{notifValues.index}}_ssn">SSN</label>
                 </div>
                 <div>
-                    <input name="member{{index}}_docs" type="checkbox" id="member{{index}}_citizenship" value="Naturalization Certificate"><label for="member{{index}}_citizenship">Naturalization Certificate</label>
+                    <input name="member{{notifValues.index}}_docs" type="checkbox" id="member{{notifValues.index}}_citizenship" value="Naturalization Certificate"><label for="member{{notifValues.index}}_citizenship">Naturalization Certificate</label>
                 </div>
                 <div>
-                    <input name="member{{index}}_docs" type="checkbox" id="member{{index}}_greencard" value="greencard"><label for="member{{index}}_greencard">Greencard</label>
+                    <input name="member{{notifValues.index}}_docs" type="checkbox" id="member{{notifValues.index}}_greencard" value="greencard"><label for="member{{notifValues.index}}_greencard">Greencard</label>
                 </div>
                 <div>
-                    <input name="member{{index}}_docs" type="checkbox" id="member{{index}}_letter" value="health coverage letter"><label for="member{{index}}_letter">Health Coverage Letter</label>
+                    <input name="member{{notifValues.index}}_docs" type="checkbox" id="member{{notifValues.index}}_letter" value="health coverage letter"><label for="member{{notifValues.index}}_letter">Health Coverage Letter</label>
                 </div>
             </fieldset>
         </fieldset>`
